cb6089e339ec056fbe634167a72e2842b9dbe55a
FrankGangWang <36325423+FrankGangWang@users.noreply.github.com>
2023-07-10 06:33:25 +0000

    add FastAPI web

--- wikibot.py
+++ wikibot.py
@@ -5,7 +5,7 @@ from mylib.bot import scrape
 # To add parameters, use the option() and argument() decorators:
 @click.command()
 @click.option("--name", prompt="Wikipedia page to scrape", help="Web page to scrape")
-@click.option("--length", help="number of sentences from wikipedia")
+@click.option("--length", prompt="Number of sentences from wikipedia", help="number of sentences from wikipedia")
 def cli(name="Microsoft", length=1):
     r = scrape(name, sentences=length)
     click.echo(click.style(f"{r}:", fg="white", bg="black"))
